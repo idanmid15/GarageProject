d7e7f8efa329e41b82bbaf91efb2b1318d23245d
IEUser <IEUser@IE11WIN10>
2016-05-22 15:07:08 -0700

    a

--- B16 Ex03 Idan 305342768 Eyal 200651669/Ex03.GarageLogic/ValueOutOfRangeException.cs
+++ B16 Ex03 Idan 305342768 Eyal 200651669/Ex03.GarageLogic/ValueOutOfRangeException.cs
@@ -23,7 +23,7 @@ namespace Ex03.GarageLogic
             }
         }
 
-        public ValueOutOfRangeException(float i_MinValue, float i_MaxValue) : base("Given Value is out of range.")
+        public ValueOutOfRangeException(float i_MinValue, float i_MaxValue) : base("Given Value is out of range")
         {
             m_MinValue = i_MinValue;
             m_MaxValue = i_MaxValue;
