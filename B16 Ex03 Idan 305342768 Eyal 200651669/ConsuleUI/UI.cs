e7f5b15793da295aaabb57e3c9f0c1754a0d36aa
IEUser <IEUser@IE11WIN10>
2016-05-28 17:34:09 +0300

    class diagram added

--- B16 Ex03 Idan 305342768 Eyal 200651669/ConsuleUI/UI.cs
+++ B16 Ex03 Idan 305342768 Eyal 200651669/ConsuleUI/UI.cs
@@ -53,7 +53,7 @@ namespace Ex03.ConsoleUI
                     //enter the new client to our data structure
                     this.m_GarageManager.AddClient(this.m_CurrentClient.m_Vehicle.GetLicensePlate(), this.m_CurrentClient);
                     OutputClearConsole();
-                    OutputToConsole(string.Format("****Thank you! We've entered your details into our system****{0}" , Environment.NewLine));
+                    OutputToConsole(string.Format("****Thank you! We've entered your details into our system****{0}", Environment.NewLine));
                 }
                 else
                 {
@@ -154,14 +154,14 @@ namespace Ex03.ConsoleUI
                 }
                 membersFromInputList.Add(parsedMemberInput);
 
-                }
+            }
             //create a new vehicle instance with all relevant params for the specific car type given
             vehicleMembersArray = membersFromInputList.ToArray();
-               
-                
+
+
             vehicleInstance = this.m_GarageManager.CreateVehicle(i_SupportedVehicle, vehicleMembersArray);
             areParametersValid = true;
-                
+
             return vehicleInstance;
         }
 
@@ -284,7 +284,8 @@ enter 2 (or 'NotInRepair') - for not in repair status"));
             {
                 OutputToConsole("Enter the amount you wish to fuel:");
             }
-            else { 
+            else
+            {
                 OutputToConsole("Enter how much time you wish to charge:");
             }
 
@@ -351,15 +352,22 @@ Enter 'Octan95' (or 1), 'Ocatan98' (or 2) or 'Soler' (or 3)"));
                 if (i_IsFuelEngine)
                 {
                     throw new Exception("Fuel amount requested is greater than the fuel tank capacity");
-                }else
+                }
+                else
                 {
                     throw new Exception("charge time requested is greater than the max charge time possible");
                 }
-            /* } else if (i_IsFuelEngine && (client.m_Vehicle.GetEngine(). != i_FuelType))
+            }
+            else if (client.m_Vehicle.m_Engine is FueledEngine)
+            {
+                FueledEngine currentFueledEngine = (FueledEngine)client.m_Vehicle.m_Engine;
+                if (i_FuelType != currentFueledEngine.GetFuelType())
                 {
                     throw new Exception("fuel type does not match the fuel type for this kind of vehicle");
-                }*/
+                }
             }
+
+
         }
 
         public void ChooseUserActions(string i_LicensePlate)
